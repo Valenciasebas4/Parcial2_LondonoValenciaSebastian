d3b816bc78afa8e67413f376b4ba5a6cc7dc9081
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-10-07 10:44:50 -0500

    Create Class DataBaseContext

---
+++ Parcial2_LondonoValenciaSebastian/Parcial2_LondonoValenciaSebastian/DAL/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_LondonoValenciaSebastian.DAL.Entities;
+
+namespace Parcial2_LondonoValenciaSebastian.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+
+        #region Properties
+        public DbSet<NaturalPerson> NaturalsPersons { get; set; }
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<NaturalPerson>().HasIndex(s => s.Id).IsUnique();
+
+
+        }
+    }
+}
