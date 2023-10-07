7b27d61df4bab7dc9c923747f006772567b63489
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-10-07 12:51:26 -0500

    Edit html

--- Parcial2_LondonoValenciaSebastian/Parcial2_LondonoValenciaSebastian/DAL/DataBaseContext.cs
+++ Parcial2_LondonoValenciaSebastian/Parcial2_LondonoValenciaSebastian/DAL/DataBaseContext.cs
@@ -9,16 +9,16 @@ namespace Parcial2_LondonoValenciaSebastian.DAL
         {
         }
 
+        //Aquí estoy mapeando mi entidad para convertirla en un DBSet (tabla)
         #region Properties
         public DbSet<NaturalPerson> NaturalsPersons { get; set; }
-
         #endregion
 
+        //Se crean los indices de las tablas
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<NaturalPerson>().HasIndex(s => s.Document).IsUnique();
-
 
         }
     }
