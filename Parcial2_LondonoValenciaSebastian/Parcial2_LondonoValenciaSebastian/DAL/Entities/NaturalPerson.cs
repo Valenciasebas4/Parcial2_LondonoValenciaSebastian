64dbdd1687f366e6919dbe23c892f9325463cdec
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-10-07 10:35:05 -0500

    Create Folder DAL and Entities, create class Entity and NaturalPerson

---
+++ Parcial2_LondonoValenciaSebastian/Parcial2_LondonoValenciaSebastian/DAL/Entities/NaturalPerson.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Parcial2_LondonoValenciaSebastian.DAL.Entities
+{
+    public class NaturalPerson : Entity
+    {
+
+        #region Properties
+
+
+        [Display(Name = "Nombre Completo")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string FullName { get; set; }
+
+
+        [Display(Name = "Correo")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Email { get; set; }
+
+        [Display(Name = "Año de Nacimiento")]
+        [MaxLength(4)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int BirthYear { get; set; }
+
+        [Display(Name = "Edad")]
+        [MaxLength(100)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int Age { get; set; }
+
+
+
+
+
+
+
+        #endregion
+    }
+}
